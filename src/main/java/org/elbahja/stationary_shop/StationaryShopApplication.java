e594b051a6ed20b4adfe80848a6bf33657028d8a
wayfarer-ismail <ismailelbahja03@gmail.com>
2024-02-21 14:55:42 +0000

    Initial commit

---
+++ src/main/java/org/elbahja/stationary_shop/StationaryShopApplication.java
@@ -0,0 +1,21 @@
+package org.elbahja.stationary_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class StationaryShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationaryShopApplication.class, args);
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/org/elbahja/stationary_shop/StationaryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elbahja.stationary_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationaryShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
