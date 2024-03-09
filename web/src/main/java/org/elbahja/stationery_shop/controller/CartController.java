6c58f4743aa63ef1542eae7bb4eaf49c0b9c2eae
wayfarer-ismail <ismailelbahja03@gmail.com>
2024-03-09 09:27:35 +0000

    added - add items to cart

--- web/src/main/java/org/elbahja/stationery_shop/controller/CartController.java
+++ web/src/main/java/org/elbahja/stationery_shop/controller/CartController.java
@@ -1,13 +1,27 @@
 package org.elbahja.stationery_shop.controller;
 
+import org.elbahja.stationery_shop.service.CartService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/cart")
 public class CartController {
+    CartService cartService;
 
-    @GetMapping("/cart")
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping
     public String cart() {
         return "cart";
+    }
+
+    @PostMapping("/add")
+    public String addToCart(@RequestParam("itemId") Long itemId) {
+        // get user id from session
+        cartService.addToCart(0L, itemId, 1);
+        return "redirect:/cart";
     }
 }
